0e11d9cb10d5d944291762feb6ca6706d334b875
Deng Balaga <dengbalaga@gmail.com>
2015-12-04 10:04:00 +0800

    add index file

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php include 'header.php' ?>
+
+
+<!-- MENU -->
+<?php include 'nav.php' ?>
+<!-- /MENU -->
+
+
+
+
+<!-- CAROUSEL -->
+<?php include 'carousel.php' ?>
+<!-- /CAROUSEL -->
+
+
+<!-- SECTION 1 -->
+<section class="page about-intro" style="padding:30px; background-color:#fff;">
+<div class="container">
+
+<div class="row">
+    <div class="col-md-6">
+    <p>&nbsp;</p>
+    <h2>Welcome to STI Education Systems Holdings Inc.</h2>
+    <p>STI Education Systems Holdings, Inc. (PSE Code: &ldquo;STI&rdquo;) is a leading education and investments corporation in the Philippines. STI Holdings is recognized as one of the largest network of schools in the country today with a total number of 80,200  students enrolled in 80 STI-branded schools nationwide composed of 66-STI branded colleges and 14-STI branded educational centers, two non-branded colleges, and one university.</p>
+    <p>In 2012, STI Holdings posted yet another milestone as it listed follow-on shares in the Philippine Stock Exchange, making it 30 percent publicly owned. The listing infused about P2.36 billion in fresh capital for the company.</p>
+	<br>
+    <a class="btn btn-blue nav-external" href="about-sti-holdings.html">Read More</a>
+    </div>
+
+    <div class="col-md-6">
+    <img src="assets/img/pse_bell.jpg" class="img-responsive" style="margin-top:40px; margin-bottom:40px; width:400px;">
+    </div>
+</div><!-- /ROW -->
+
+</div>
+</section>
+<!-- /SECTION 1 -->
+
+
+
+
+<!-- SECTION 2 -->
+<section id="clients" class="page" style="background-attachment:fix;">
+      <div class="container">
+
+       <div class="row">
+
+       		<div class="col-md-7">
+            
+            	<p>&nbsp;</p>
+            
+            </div>
+       
+       		<div class="col-md-5">
+                <div class="content">
+                <div class="heading">
+                    <h2 class="title-shadow">Corporate Social Responsiblity</h2>
+                <!--    <div class="border"></div>-->
+                    <p class="lead title-shadow">Since SY 2010-11, STI ESG has supported the advocacy of ABS-CBN Foundation’s Kapit Bisig Para sa Ilog Pasig, which aims to raise awareness and funds for the rehabilitation of Ilog Pasig and the esteros connected to it.</p>
+<!--                <p>Culpa, alias enim placeat earum quos ab. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, alias enim placeat earum quos ab.</p>-->
+                    <p>&nbsp;</p>
+                     <a href="csr.html" class="btn btn-blue">Read More</a>
+                  </div>
+    
+                    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                </div>
+            </div>
+            
+		</div>
+
+      </div>
+    </section>
+<!-- / SECTION 2 -->
+
+
+
+<!-- SECTION 3 -->
+   <section id="investorrelations" class="page">
+       <div class="container">
+            <div class="content text-center">
+           <div class="heading">
+                <h2>Press Materials / News</h2>
+                <div class="border"></div>
+                <p><a href="news.html">Click here</a> for more STI news.</p>
+                <p>&nbsp;</p>
+           </div>
+	
+    		<div class="col-sm-4">
+           	  <img src="assets/img/article/article-img1.jpg" class="img-responsive img-thumbnail" alt=""/>
+              <h5>76 STI schools ready to offer senior high school program under K to 12</h5>
+              <a href="news-inside.html" class="btn btn-blue">Read More</a>
+            </div>
+    		<div class="col-sm-4">
+           	  <img src="assets/img/article/article-img2.jpg" class="img-responsive img-thumbnail" alt=""/>
+              <h5>STI and ABS-CBN, Now on 7th Halalan Partnership</h5>
+                <a href="news-inside.html" class="btn btn-blue">Read More</a>
+            </div>
+    		<div class="col-sm-4">
+           	  <img src="assets/img/article/article-img3.jpg" class="img-responsive img-thumbnail" alt=""/>
+              <h5 class="text-left">STI Holdings revenue breaks P2 billion mark as enrollments drive more students</h5>
+                <a href="news-inside.html" class="btn btn-blue">Read More</a>
+            </div>
+				<p>&nbsp;</p>
+			</div>
+		</div>
+	</section>
+<!-- /SECTION 3 -->
+
+
+
+<!-- SECTION 4 -->
+    <section id="contact" class="page" style="background-color:#f9f9f9;">
+      <div class="container">
+       <div class="content text-center">
+           <div class="heading">
+            <h2>Contact Us</h2>
+            <div class="border"></div>
+            <p>&nbsp;</p>           
+            <div class="col-sm-6 text-left" style="margin-bottom:70px;">
+<p class="lead thin" style=" margin-top:0px;">The headquarters of STI Holdings is located at 7F iACADEMY Building, 6764 Ayala Avenue, Makati City 1226. </p>
+<p>
+<i class="fa fa-hourglass" style="line-height:16px; margin-top:12px;"></i> &nbsp;<b>Office Hours:</b> 9:00 AM - 6:00 PM, Mondays to Fridays<br>
+<i class="fa fa-phone" style="line-height:16px; margin-top:0px;"></i> &nbsp;<b>Telephone:</b> +632 844-9553<br>
+<i class="fa fa-fax" style="line-height:16px; margin-top:0px;"></i> &nbsp;<b>Fax:</b> +632 844-9553<br>
+<i class="fa fa-envelope" style="line-height:16px; margin-top:0px;"></i> &nbsp;<b>Email:</b> info@stiholdings.com.ph</p>
+<p>&nbsp;</p>
+            </div>
+            
+            <div class="col-sm-6">
+            
+			<iframe src=		"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.8758350756823!2d121.02098600401328!3d14.556188500000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c90f7b679515%3A0xd814856c9463eb63!2sSti+Education+Services+Group!5e0!3m2!1sen!2sph!4v1447819131996" width="100%" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+            
+            </div>
+            
+          </div>
+
+        </div>
+      </div>
+    </section>
+<!-- /SECTION 4 -->
+
+
+
+
+<!-- LOGOS -->
+<?php include 'logos.php' ?>
+<!-- /LOGOS -->
+
+
+
+
+<!-- FOOTER -->
+<?php include 'footer.php' ?>
+<!-- / FOOTER -->
+ 
+  </body>
+</html>
